0f0f82f47335a5a21b750d946f9d73414621452b
Chika0221 <chikai400up@gmail.com>
2026-07-04 01:39:24 +0900

    feat: 環境変数を管理するenv.tsファイルを追加

---
+++ apps/api/src/env.ts
@@ -0,0 +1,3 @@
+export const DISCORD_TOKEN = process.env.DISCORD_TOKEN || ""
+export const DISCORD_CLIENT_ID = process.env.DISCORD_CLIENT_ID || ""
+export const DISCORD_CHANNEL_ID = process.env.DISCORD_CHANNEL_ID || ""
